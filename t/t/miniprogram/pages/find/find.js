68c79915ac5954980e998f3934e48e66cccb6940
15017365028 <1410167806@qq.com>
2021-06-17 16:41:35 +0800

    微信小程序代码
    微信小程序代码

---
+++ t/t/miniprogram/pages/find/find.js
@@ -0,0 +1,70 @@
+// pages/find/find.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    scrArr : [
+      "https://imgcps.jd.com/ling4/100006061653/6I2J5pys5oqk6IKk/5aGR6YCg5YGl5bq3576O6IKM/p-5bd8253082acdd181d02fa68/9f29e207/cr/s/q.jpg",
+      "https://imgcps.jd.com/ling4/2922989/6LaF5YC85aW954mp5oOg5LiN5YGc/5aW96Iy25aW95rC05aW955Sf5rS7/p-5bd8253082acdd181d02fa42/fc10af46/cr/s/q.jpg",
+      "https://img11.360buyimg.com/pop/s590x470_jfs/t1/128317/6/16862/34674/5f9e2a83Ebb12340a/0c1aa74603807876.jpg.webp"
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ t/t/miniprogram/pages/test/test.js
@@ -0,0 +1,77 @@
+// pages/test/test.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    flag : true,
+    title : "hello world!",
+    arr : [
+      {
+        src : "https://img14.360buyimg.com/babel/s590x470_jfs/t1/159584/23/1620/101334/5ff307f5E1c4e7e8d/48bd9a17d204eb84.jpg.webp",
+        text : "1.8plus"
+      },
+      {
+        src : "https://img14.360buyimg.com/da/s590x470_jfs/t1/126707/32/7181/82882/5f0efab9E90f1bb8e/e8bec706499de595.jpg.webp",
+        text : "三只松鼠"
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
